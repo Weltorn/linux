633f775465a86ccbc1acf05bec9a171882631dac
Weltorn <2029321665@qq.com>
2019-11-11 09:40:04 +0800

    week10 day1

---
+++ week10/vforktest/vfork.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+ int g=88;
+ int main()
+{
+ int v=99;
+ pid_t pid;
+ pid=vfork();
+ if(pid<0)
+{
+ perror("vfork failed\n");
+ exit(-1);
+}
+ else if(pid==0)
+{
+g++;
+v++;
+ printf("child :pid=%d,g=%d, v=%d\n",getpid(),g, v);
+ return 0;
+}
+ printf("parent: id=%d,g=%d.v=%d\n",getpid(),g,v);
+return 0;
+}
